c39312f5727d1a9ffd5ae79daa1c376ff73b70e7
Mahavir Jain <mahavir.jain@infobeans.com>
2019-09-11 11:07:10 +0530

    comment modified

--- WindowsCalculator/WindowsCalculator.iOS/Main.cs
+++ WindowsCalculator/WindowsCalculator.iOS/Main.cs
@@ -4,7 +4,7 @@ namespace WindowsCalculator.iOS
 {
 	public class Application
 	{
-		// This is the main entry point of the application.
+		// Program starts here.
 		static void Main(string[] args)
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
